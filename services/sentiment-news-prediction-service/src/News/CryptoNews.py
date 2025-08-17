af4aceb3c809258be2c05ee6972dee11c0f82c00
ketamean <truongthanhtoan2003@gmail.com>
2025-08-17 18:59:55 +0700

    [u] edit CryptoNews.py package import

--- services/sentiment-news-prediction-service/src/News/CryptoNews.py
+++ services/sentiment-news-prediction-service/src/News/CryptoNews.py
@@ -1,7 +1,7 @@
 from .INews import INews
 
 class CryptoNews(INews):
-  def __init__(self, subject: str, text: str, title: str):
+  def __init__(self, subject: str, text: str, title: str, sentiment: int | None = None):
     """
     Initializes a new instance of the CryptoNews class.
     :param subject: The subject (related field, such as bitcoin, nft, altcoin,...) of the news.
@@ -11,6 +11,7 @@ class CryptoNews(INews):
     self.__subject = subject
     self.__text = text
     self.__title = title
+    self.__sentiment = sentiment
 
   @property
   def subject(self):
@@ -23,6 +24,14 @@ class CryptoNews(INews):
   @property
   def title(self):
     return self.__title
+  
+  @property
+  def sentiment(self):
+    return self.__sentiment
+  @sentiment.setter
+  
+  def sentiment(self, value: int):
+    self.__sentiment = value
   
   def __str__(self):
     return f"CryptoNews(subject={self.__subject}, text={self.__text}, title={self.__title})"
