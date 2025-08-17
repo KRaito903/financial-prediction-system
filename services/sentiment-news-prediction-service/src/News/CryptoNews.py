d0a15b2bd80b295db6587400465202fa2c3c3188
ketamean <truongthanhtoan2003@gmail.com>
2025-08-17 17:44:59 +0700

    [+] News: dataclass for news to be predicted

---
+++ services/sentiment-news-prediction-service/src/News/CryptoNews.py
@@ -0,0 +1,28 @@
+from .INews import INews
+
+class CryptoNews(INews):
+  def __init__(self, subject: str, text: str, title: str):
+    """
+    Initializes a new instance of the CryptoNews class.
+    :param subject: The subject (related field, such as bitcoin, nft, altcoin,...) of the news.
+    :param text: The brief text content of the news.
+    :param title: The title of the news.
+    """
+    self.__subject = subject
+    self.__text = text
+    self.__title = title
+
+  @property
+  def subject(self):
+    return self.__subject
+  
+  @property
+  def text(self):
+    return self.__text
+  
+  @property
+  def title(self):
+    return self.__title
+  
+  def __str__(self):
+    return f"CryptoNews(subject={self.__subject}, text={self.__text}, title={self.__title})"
